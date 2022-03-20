93959fbab53293d57ffc893632eaa931ccc9d988
Richard Matsumoto <richard.matsumoto@gmail.com>
2022-03-20 12:21:53 -0400

    add Woodwind

---
+++ src/orchestra/section/Woodwind.java
@@ -0,0 +1,14 @@
+package orchestra.section;
+
+public class Woodwind extends Section {
+
+    public Woodwind() {
+        super("Woodwind");
+        addInstrucment("Flute");
+        addInstrucment("Oboe");
+        addInstrucment("Clarinet");
+        addInstrucment("Bassoon");
+        addInstrucment("Saxophone");
+        addInstrucment("Piccolo");
+    }
+}
